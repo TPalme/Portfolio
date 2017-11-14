2e08a315f8d5c01d1c65656d90ca19879fcc501c
TPalme <thepalme@gmail.com>
2017-11-13 18:57:14 -0800

    done 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
+<!--//stackoverflow.com/questions/3264739/image-change-every-30-seconds-loop -->
+<script language="javascript" type="text/javascript">
+ var images = new Array()
+ images[0] = "img/logo1.png";
+ images[1] = "img/logo1blue.png";
+ images[2] = "img/logo1.png";
+ setInterval("changeImage()", 200);
+ var x=0;
 
+ function changeImage()
+ {
+            document.getElementById("img").src=images[x]
+            x++;
+            if (images.length == x)
+            {
+                x = 0;
+            }
+ }
+</script>
